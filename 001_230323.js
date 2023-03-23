70c6804dd81d0e49e57e2b970d34ff8304789f15
Roufujjaman <rrhtcode@gmail.com>
2023-03-23 22:44:59 +0600

    23.03.23

---
+++ 001_230323.js
@@ -0,0 +1,9 @@
+function multiplicationTable(num){
+    for (let i=1; i<11; i++){
+        console.log(`num x iteration = ${i*num}`);
+    }
+}
+
+
+multiplicationTable(5);
+multiplicationTable(10);
