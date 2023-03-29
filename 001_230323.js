97cae8a507015a469eec7b25fcd3685632b1a715
Roufujjaman <rrhtcode@gmail.com>
2023-03-29 21:02:39 +0600

    29.3.23

--- 001_230323.js
+++ 001_230323.js
@@ -1,9 +1,1 @@
-function multiplicationTable(num){
-    for (let i=1; i<11; i++){
-        console.log(`num x iteration = ${i*num}`);
-    }
-}
-
-
-multiplicationTable(5);
-multiplicationTable(10);
+empty
---
+++ BMI Calculator/script.js
@@ -0,0 +1,6 @@
+let result = document.getElementById("bmi")
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", () => {
+    result.innerHTML = "123132"
+});
